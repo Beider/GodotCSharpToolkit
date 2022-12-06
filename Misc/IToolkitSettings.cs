ebbd55db3bc126d4a68ece52738037f325e57734
Beider <vegarste@gmail.com>
2022-12-06 16:45:02 +0100

    Updated documentation

--- Misc/IToolkitSettings.cs
+++ Misc/IToolkitSettings.cs
@@ -10,11 +10,14 @@ namespace GodotCSharpToolkit.Misc
     {
         /// <summary>
         /// Should return the absolute path to your project directory
+        /// eg. C:/Coding/<my game folder>/"
         /// </summary>
         string GetProjectAbsolutePath();
 
         /// <summary>
-        /// Path where we should store data files
+        /// Path where we should store data files (relative to the absolute path)
+        /// eg. Data/Toolkit/
+        /// The main thing stored here will be event IDs for the event system
         /// </summary>
         string GetDataPath();
     }
